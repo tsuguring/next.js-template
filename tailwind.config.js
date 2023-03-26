7fffc12eb37b4c1a94149a6108c464d758660630
tsuguri <tsuguri4429@gmail.com>
2023-03-26 15:27:26 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+      './src/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      // https://purgecss.com/safelisting.html#patterns
+      safelist: {
+        standard: [/^bg-/, /^text-/],
+      },
+    },
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
